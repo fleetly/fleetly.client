b728d801f074c1c08f87c18cd69c71fee8830dda
Ivan Patafeev <ivan.patafeev@dnateam.ru>
2020-04-29 09:01:13 +0300

    [FL-47] Add asyncValidate util

---
+++ src/components/Form/utils/asyncValidate.ts
@@ -0,0 +1,21 @@
+import { get, set } from 'lodash';
+
+interface ISchema {
+  validate: (values: any, options: any) => Promise<any>;
+}
+
+interface IError {
+  message: string;
+  path: string;
+}
+
+export default (schema: ISchema) => (values: object) =>
+  schema.validate(values, { abortEarly: false }).catch((error: object) => {
+    const errors = {};
+
+    get(error, 'inner', []).forEach(({ message, path }: IError) => {
+      set(errors, path, message);
+    });
+
+    throw errors;
+  });
