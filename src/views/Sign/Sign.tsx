00c78af50852747c158e9f7109894a427ace016d
Ivan Vyatkin <vanya095@gmail.com>
2020-04-30 18:55:09 +0300

    Add animations.

--- src/views/Sign/Sign.tsx
+++ src/views/Sign/Sign.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { CSSTransition } from 'react-transition-group';
 
 // Components
 import { H1, H2 } from '@components/Typography';
@@ -6,15 +7,37 @@ import { H1, H2 } from '@components/Typography';
 // Styles
 import styles from './Sign.scss';
 
-const Sign = () => (
-  <div className={styles.Root}>
-    <div className={styles.Wrapper}>
-      <div className={styles.Content}>
-        <H2 className={styles.Welcome}>Welcome to</H2>
-        <H1 className={styles.Title}>Fleetly</H1>
+const Sign = () => {
+  const [isMounted, setMountState] = React.useState(false);
+
+  React.useEffect(() => setMountState(true), []);
+
+  return (
+    <CSSTransition
+      classNames={{
+        enter: styles.RootTransitionEnter,
+        enterActive: styles.RootTransitionEnterActive
+      }}
+      in={isMounted}
+      mountOnEnter={true}
+      timeout={2800}
+    >
+      <div className={styles.Root}>
+        <div className={styles.Cover}>
+          <div className={styles.Land1} />
+          <div className={styles.Land2} />
+          <div className={styles.Tree} />
+        </div>
+
+        <div className={styles.Wrapper}>
+          <div className={styles.Content}>
+            <H2 className={styles.Welcome}>Welcome to</H2>
+            <H1 className={styles.Title}>Fleetly</H1>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-);
+    </CSSTransition>
+  );
+};
 
 export default Sign;
