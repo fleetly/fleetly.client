fd0fecd2f049aa3c5bac8f897b29af3de24a2bee
Ivan Vyatkin <vanya095@gmail.com>
2020-12-05 13:21:57 +0300

    Added pushError notification.

--- src/components/Form/utils/gqlErrorHandler.ts
+++ src/components/Form/utils/gqlErrorHandler.ts
@@ -1,49 +1,10 @@
-import { get, set } from 'lodash';
-import { GraphQLFormattedError } from 'graphql';
+import { ApolloError } from 'apollo-boost';
 import { SubmissionError } from 'redux-form';
 
 // Utils
-import { capitalizeFirstLetter } from '@utils/string';
+import { parseGQLError } from '@utils/graphql';
 
-const formatValidationErrors = (validationErrors: any[]) => {
-  const result: any = {};
-
-  Object.keys(validationErrors).forEach((key: any) => {
-    const error = validationErrors[key];
-
-    result[key] =
-      typeof error === 'string' ? error : formatValidationErrors(error);
-  });
-
-  return result;
-};
-
-export default ({ graphQLErrors, message, networkError }: any): void => {
-  let errors = {};
-
-  if (graphQLErrors && graphQLErrors.length > 0) {
-    graphQLErrors.forEach(({ extensions, message }: GraphQLFormattedError) => {
-      const validationErrors = get(
-        extensions,
-        'exception.response.validationErrors'
-      );
-
-      if (validationErrors) {
-        const formattedErrors = formatValidationErrors(validationErrors);
-        errors = { ...errors, ...formattedErrors };
-      }
-
-      if (message) {
-        set(errors, '_error', capitalizeFirstLetter(message));
-      }
-    });
-  } else if (message) {
-    set(errors, '_error', capitalizeFirstLetter(message));
-  }
-
-  if (networkError) {
-    set(errors, '_error', capitalizeFirstLetter(networkError.message));
-  }
-
-  throw new SubmissionError(errors);
+export default (error: ApolloError): void => {
+  const { message, validationErrors } = parseGQLError(error);
+  throw new SubmissionError({ ...validationErrors, _error: message });
 };
---
+++ src/utils/graphql.ts
@@ -0,0 +1,39 @@
+import { ApolloError } from 'apollo-client';
+import { get } from 'lodash';
+
+// Utils
+import { capitalizeFirstLetter } from '@utils/string';
+
+export const parseGQLError = (error: ApolloError) => {
+  let message = error.networkError?.message || error.message;
+  let validationErrors: Record<string, string> = {};
+
+  const formatValidationErrors = (validationErrors: any[]) => {
+    const result: any = {};
+
+    Object.keys(validationErrors).forEach((key: any) => {
+      const error = validationErrors[key];
+
+      result[key] =
+        typeof error === 'string' ? error : formatValidationErrors(error);
+    });
+
+    return result;
+  };
+
+  if (error.graphQLErrors && error.graphQLErrors.length > 0) {
+    // @todo - Then expand on many errors
+    const { extensions, message: eMessage } = error.graphQLErrors[0];
+
+    validationErrors = formatValidationErrors(
+      get(extensions, 'exception.response.validationErrors', [])
+    );
+
+    message = eMessage;
+  }
+
+  return {
+    message: capitalizeFirstLetter(message),
+    validationErrors
+  };
+};
