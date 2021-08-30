89766edde8439baab238d3b267c2afaa5c6aaa6b
Ivan Vyatkin <vanya095@gmail.com>
2021-08-30 13:14:36 +0300

    Fixed naiming bug.

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-import '@utils/charts';
+import './Core/utils/charts';
 
 // Apollo
 import {
